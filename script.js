955023d7bcddd16a704ba0246a947715402bf3eb
Rafał Mucha <r.p.mucha@gmail.com>
2022-03-11 02:53:07 +0100

    Kinda done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var count = 0;
 var scream = new Audio("./Assets/GIRLS.wav");
+var cards = document.getElementsByClassName("card");
+var dots = document.getElementsByClassName("dots");
 
 function next() {
   count++;
-  let cards = document.getElementsByClassName("card");
   for (c of cards) {
     c.style.display = "none";
   }
-  let dots = document.getElementsByClassName("dots");
   for (d of dots) {
     d.classList.remove("active");
   }
@@ -17,11 +17,9 @@ function next() {
 
 function prev() {
   count--;
-  let cards = document.getElementsByClassName("card");
   for (c of cards) {
     c.style.display = "none";
   }
-  let dots = document.getElementsByClassName("dots");
   for (d of dots) {
     d.classList.remove("active");
   }
@@ -35,6 +33,18 @@ function zombie() {
   scream.volume = 1.0;
   setTimeout(() => {
     document.getElementById("zombie").style.display = "none";
-    window.location.href = "https://rafal-mucha.pl";
+    count = 0;
+    for (c of cards) {
+      c.style.display = "none";
+    }
+    for (d of dots) {
+      d.classList.remove("active");
+    }
+    document.getElementById("dot-" + count).classList.add("active");
+    document.getElementsByClassName("card")[count].style.display = "block";
   }, 1500);
+}
+
+function redir() {
+  window.location.href = "https://rafal-mucha.pl";
 }
